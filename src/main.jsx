168430ddc348b5baca678de4cf9d99ec40ef9dcb
D3VPAND3Y <dev16pandey@gmail.com>
2023-06-11 00:15:13 +0530

    Finalizing

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-  <BrowserRouter basename="/Portfolio">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 
   </React.StrictMode>,
   rootElement
