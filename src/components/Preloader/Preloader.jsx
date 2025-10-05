dff033fd369594db18eb5abd0b2ad9da926b0364
devpand <devpand@cisco.com>
2025-10-05 22:26:53 +0530

    Fixed the loader issue it reloads once per user visit

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,16 +1,21 @@
-// import React from 'react'
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import "./Preloader.styles.scss"
 import { preLoaderAnim } from "../../animations"
 
-
 const Preloader = () => {
+  const [showPreloader, setShowPreloader] = useState(() => {
+    return !sessionStorage.getItem('preloaderShown');
+  });
 
   useEffect(() => {
-    preLoaderAnim();
-  }, [])
+    if (showPreloader) {
+      preLoaderAnim();
+    }
+  }, [showPreloader]);
 
-
+  if (!showPreloader) {
+    return null;
+  }
 
   return (
     <div className="preloader">
@@ -18,10 +23,8 @@ const Preloader = () => {
         <span>Learn.</span>
         <span>Build.</span>
         <span>Craft.</span>
-
         </div>
     </div>
-
   )
 }
 
